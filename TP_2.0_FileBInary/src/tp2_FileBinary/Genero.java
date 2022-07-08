8ad81d63b033c6e2c3d5a58b435d04824cf2f891
AgustinValdes299 <74334826+AgustinValdes299@users.noreply.github.com>
2022-07-08 19:23:22 -0300

    Add files via upload

---
+++ TP_2.0_FileBInary/src/tp2_FileBinary/Genero.java
@@ -0,0 +1,12 @@
+package tp2_FileBinary;
+
+/**
+ *
+ * @author PC ARI Central
+ */
+public enum Genero {
+    Masculino,
+    Femenino,
+    SG;
+
+}
---
+++ TP_2.0_FileBInary/src/tp2_FileBinary/TipoMembrecia.java
@@ -0,0 +1,14 @@
+package tp2_FileBinary;
+
+/**
+ *
+ * @author Software-Notebook
+ */
+public enum TipoMembrecia {
+    Bronce,
+    Plata,
+    Oro,
+    Platino,
+    Black;
+}
+
